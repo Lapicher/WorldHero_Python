d2a5a6e9eb6e46e4eaf4b85c06f35d2f047be660
Nataly Contreras <nataly.contreras@cidesi.edu.mx>
2016-10-01 15:28:15 -0500

    se crean los modelos de blog y cateorias, se agregan al administrador django para tambien manejarlas desde ahi

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from categorias.models import Category
+
+
+class Blog(models.Model):
+
+    owner = models.ForeignKey(User)
+    type = models.ForeignKey(Category)
+    title = models.CharField(max_length=100)
+    intro = models.CharField(max_length=400)
+    body = models.TextField()
+    imageUrl = models.URLField()
+    datePub = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+urlpatterns = [
+    #url(r'^blogs/(?P<pk>[0-9]+)$', PhotoDetailView.as_view(), name='photos_detail'),
+    #url(r'^create$', PhotoCreationView.as_view(), name='photos_create'),
+    #url(r'^photos/$', PhotoListView.as_view(), name='photos_my_photos'),
+    #url(r'^$', HomeView.as_view(), name='photos_home'),
+]
