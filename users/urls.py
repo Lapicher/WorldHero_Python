345a8645d30d1ff996c94cff4452aa39ca993dd4
Nataly Contreras <nataly.contreras@cidesi.edu.mx>
2016-10-17 09:35:52 -0500

    se cambia register por signup

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from users.views import UserRegisterView, ActualizarUser, LoginView, LogoutView
 
 patrones_user = [
 
-    url(r'^register$', UserRegisterView.as_view(), name="register_user"),
+    url(r'^signup$', UserRegisterView.as_view(), name="register_user"),
     url(r'^blogs/(?P<user>[a-zA-Z0-9]+)/updateUser$', ActualizarUser.as_view(), name="update_user"),
 
     url(r'^login$', LoginView.as_view(), name="login"),
