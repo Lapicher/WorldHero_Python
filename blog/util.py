25541b88c2fa1fd8548b473fe50bf991a782a4c9
Nataly Contreras <nataly.contreras@cidesi.edu.mx>
2016-11-04 23:17:48 -0600

    Command de DJANGO para ejecutarlo y se encargue de generar las imagenes responsive. Se modifica el metodo de generar las imagenes para que actualice la bandera en el campo de image_resized

--- blog/util.py
+++ blog/util.py
@@ -3,3 +3,5 @@ from easy_thumbnails.files import generate_all_aliases
 
 def generate_responsive_images(post):
     generate_all_aliases(post.image, True)
+    post.image_resized = True
+    post.save()
