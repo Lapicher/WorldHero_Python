305f701e8c9ba875c2e64ecefbfb31b3ff4845e2
Nataly Contreras <nataly.contreras@cidesi.edu.mx>
2017-02-22 00:53:43 -0600

    mas url del sitio en los href

--- src/js/header-events.js
+++ src/js/header-events.js
@@ -8,12 +8,12 @@ $('.icon-lupa').on("click", eventoLupa);
 $('.txt-buscar').on("keypress", function(evt){
     var dato_busqueda = $(this).val();
     if(evt.key == 'Enter')
-        window.location = "./?search="+dato_busqueda;
+        window.location = "http://nataly.cloudapp.net/python/blogs/?search="+dato_busqueda;
 });
 
 $('.titulo').on("click", function(){
 
-    window.location = "/python";
+    window.location = "http://nataly.cloudapp.net/python/";
 });
 
 
